22e83fb17a1c4b86d661d08f83962a6dd607e335
Evgeniy Panov <faydaen@inbox.ru>
2020-03-17 23:13:19 +0300

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Шахмотные дебюты</title>
+        <link rel="stylesheet" href="/node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.min.css">
+        <script src="/node_modules/jquery/dist/jquery.slim.min.js"></script>
+        <script src="/node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.min.js"></script>
+    </head>
+    <body>
+
+        <div id="board1" style="width: 400px"></div>
+
+        <script type="text/javascript">
+            var board1 = Chessboard('board1', 'start')
+        </script>
+    </body>
+</html>
