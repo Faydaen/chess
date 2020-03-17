8a212bc6b13ea852848c4c16c124b896463bb325
Evgeniy Panov <faydaen@inbox.ru>
2020-03-18 00:39:22 +0300

    пример дерева для jsTree

---
+++ README.md
@@ -0,0 +1,3 @@
+
+#### ссылки
+[формат json для jsTree](https://www.jstree.com/docs/json/)
--- data/ajax.php
+++ data/ajax.php
@@ -4,10 +4,25 @@
 $data = [
     [
         "id" => 1,
-        "text" => "test",
+        "text" => "e4 e5 - открытый дебют",
         "children" => false
-    ]
-
+    ],
+    [
+        "id" => 2,
+        "text" => "d4 d5 - закрытый дебют",
+        "state" =>     [
+            "opened"    => true ,
+            "disabled"  => false,
+            "selected"  => false,
+        ],
+        "children" => [
+            [
+                "id" => 21,
+                "text" => "c4 * - ферзевой гамбит",
+                "children" => false
+            ],
+        ]
+    ],
 ];
 
 header("Content-type: application/json; charset=utf-8");
