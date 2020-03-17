43c54165b62b84973379ccc1fe275a2904f8c5dc
Evgeniy Panov <faydaen@inbox.ru>
2020-03-17 23:36:51 +0300

    доска в начальной позиции

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
-<html lang="ru" dir="ltr">
+<html lang="ru">
     <head>
         <meta charset="utf-8">
         <title>Шахмотные дебюты</title>
-        <link rel="stylesheet" href="/node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.min.css">
-        <script src="/node_modules/jquery/dist/jquery.slim.min.js"></script>
-        <script src="/node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.min.js"></script>
+        <!-- взято от сюда https://www.flaticon.com/packs/chess-3 -->
+        <link rel="shortcut icon" href="/favicon.png" type="image/png">
+
+        <link rel="stylesheet" href="node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.min.css">
+        <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+        <script src="node_modules/@chrisoakman/chessboardjs/dist/chessboard-1.0.0.min.js"></script>
     </head>
     <body>
 
-        <div id="board1" style="width: 400px"></div>
+        <div id="board" style="width: 400px"></div>
 
         <script type="text/javascript">
-            var board1 = Chessboard('board1', 'start')
+            // /home/ghost/web/app/chess/bN.png
+            var config = {
+                pieceTheme: '/chesspieces/wikipedia/{piece}.png',
+                position: 'start'
+            };
+
+            var board1 = Chessboard('board',config)
         </script>
     </body>
 </html>
